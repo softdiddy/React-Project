9d5619b4152a6f2b6852dc636a7a277e6a1f7478
Mohammed Lawal Abubakar <softdiddy@gmail.com>
2021-11-15 00:57:24 +0100

    Initial commit

---
+++ studentportal/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Go</h1>
+        <Button>Go</Button>
+    </div>
+  );
+}
+
+export default App;
